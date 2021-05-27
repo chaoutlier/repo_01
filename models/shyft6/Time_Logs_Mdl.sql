0634655c24904be5890ecbe62bfd3ba0abe8e765
will@chaogrowth.com <will@chaogrowth.com>
2021-05-27 07:40:54 +0000

    commit 20210527 1240

--- models/shyft6/Time_Logs_Mdl.sql
+++ models/shyft6/Time_Logs_Mdl.sql
@@ -4,22 +4,55 @@
 )}}
 
 
+-- select
+--     -- count(1)
+--     -- c.Job_Start_Date,
+--     a.*
+-- from (
+--     select
+--         -- b.Job_Name Job_Name_Name,
+--         if(b.Job_Name  LIKE '%Regular%', 'RT',
+--             if(b.Job_Name  LIKE '%OT%', 'OT',
+--                 if(b.Job_Name  LIKE '%DT%', 'DT',NULL)
+--                 )
+--             ) Job_Type,
+--         a.*
+--     from `gentle-charmer-297601.dbt_wchen_shyft6.Time_Logs` a
+--     left join `gentle-charmer-297601.dbt_wchen_shyft6.Jobs` b
+--         on a.Job_Name = b.ID
+--     ) a
+-- -- left join {{ref('linkJobStartDateOfTheJob')}} c
+-- --     on a.User_Name =c.User_Name and a.Project =c.Project and a.Date =c.Date and a.Job_Type =c.Job_Type
+
+
+
 select
-    -- count(1)
-    c.Job_Start_Date,
+    b.Job_Start_Date_RT,
+    b.Job_Start_Date_OT,
+    b.Job_Start_Date_DT,
+    c.RPH_fromAssignees RPH_fromAssignees_RT,
+    c.RPH_fromJobs RPH_fromJobs_RT,
+    d.RPH_fromAssignees RPH_fromAssignees_OT,
+    d.RPH_fromJobs RPH_fromJobs_OT,
+    e.RPH_fromAssignees RPH_fromAssignees_DT,
+    e.RPH_fromJobs RPH_fromJobs_DT,
     a.*
 from (
     select
-        -- b.Job_Name Job_Name_Name,
-        if(b.Job_Name  LIKE '%Regular%', 'RT',
-            if(b.Job_Name  LIKE '%OT%', 'OT',
-                if(b.Job_Name  LIKE '%DT%', 'DT',NULL)
-                )
-            ) Job_Type,
-        a.*
-    from `gentle-charmer-297601.dbt_wchen_shyft6.Time_Logs` a
-    left join `gentle-charmer-297601.dbt_wchen_shyft6.Jobs` b
-        on a.Job_Name = b.ID
+        User_Name,
+        Project,
+        Date,
+        sum(Hours) sum_Hours
+    from `gentle-charmer-297601.dbt_wchen_shyft6.Time_Logs`
     ) a
-left join {{ref('linkJobStartDateOfTheJob')}} c
-    on a.User_Name =c.User_Name and a.Project =c.Project and a.Date =c.Date and a.Job_Type =c.Job_Type
+left join {{ref('linkJobStartDateOfTheJob')}} b
+    on a.User_Name =b.User_Name and a.Project =b.Project and a.Date =b.Date
+left join {{ref('rateTable01')}} c
+    on a.User_Name =c.User and a.Project =c.Project and b.Job_Start_Date_RT =c.Job_Start_Date
+left join {{ref('rateTable01')}} d
+    on a.User_Name =d.User and a.Project =d.Project and b.Job_Start_Date_OT =d.Job_Start_Date
+left join {{ref('rateTable01')}} e
+    on a.User_Name =e.User and a.Project =e.Project and b.Job_Start_Date_DT =e.Job_Start_Date
+
+
+
--- models/shyft6/linkJobStartDateOfTheJob.sql
+++ models/shyft6/linkJobStartDateOfTheJob.sql
@@ -39,12 +39,51 @@
 --     where b.Job_Name  LIKE '%DT%'
 --     group by 1,2,3,4
 
+
+----------------------------------------------------------------------------------------------------------------
+-- SELECT
+--     a.User_Name,
+--     a.Project,
+--     a.Date,
+
+--     sum(a.Hours) sum_Hours,
+
+--     max(b.Job_Start_Date) Job_Start_Date_RT,
+--     max(c.Job_Start_Date) Job_Start_Date_OT,
+--     max(d.Job_Start_Date) Job_Start_Date_DT,
+
+--     max(b.RPH_fromJobs) RPH_fromJobs_RT,
+--     max(c.RPH_fromJobs) RPH_fromJobs_OT,
+--     max(d.RPH_fromJobs) RPH_fromJobs_DT,
+
+--     max(b.RPH_fromAssignees) RPH_fromAssignees_RT,
+--     max(c.RPH_fromAssignees) RPH_fromAssignees_OT,
+--     max(d.RPH_fromAssignees) RPH_fromAssignees_DT,
+
+-- FROM `gentle-charmer-297601.dbt_wchen_shyft6.Time_Logs` a
+-- left join {{ref('rateTable01')}} b
+--     on a.User_Name=b.User and a.Project= b.Project and a.Date>= b.Job_Start_Date
+-- left join {{ref('rateTable01')}} c
+--     on a.User_Name=c.User and a.Project= c.Project and a.Date>= c.Job_Start_Date
+-- left join {{ref('rateTable01')}} d
+--     on a.User_Name=d.User and a.Project= d.Project and a.Date>= d.Job_Start_Date
+
+-- where
+--         b.Job_Name  LIKE '%Regular%'
+--     and c.Job_Name  LIKE '%OT%'
+--     and d.Job_Name  LIKE '%DT%'
+-- group by 1,2,3
+
+
+----------------------------------------------------------------------------------------------------------------
 SELECT
-    -- "RT" Job_Type,
     a.User_Name,
     a.Project,
     a.Date,
-    max(b.Job_Start_Date) Job_Start_Date
+    max(b.Job_Start_Date) Job_Start_Date_RT,
+    max(c.Job_Start_Date) Job_Start_Date_OT,
+    max(d.Job_Start_Date) Job_Start_Date_DT,
+
 FROM `gentle-charmer-297601.dbt_wchen_shyft6.Time_Logs` a
 left join {{ref('rateTable01')}} b
     on a.User_Name=b.User and a.Project= b.Project and a.Date>= b.Job_Start_Date
@@ -52,6 +91,7 @@ left join {{ref('rateTable01')}} c
     on a.User_Name=c.User and a.Project= c.Project and a.Date>= c.Job_Start_Date
 left join {{ref('rateTable01')}} d
     on a.User_Name=d.User and a.Project= d.Project and a.Date>= d.Job_Start_Date
+
 where
         b.Job_Name  LIKE '%Regular%'
     and c.Job_Name  LIKE '%OT%'
